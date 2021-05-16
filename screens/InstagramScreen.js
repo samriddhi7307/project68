035ec1b495c9fc6273ae346c9a291bde0a6093ad
samriddhi7307 <72978417+samriddhi7307@users.noreply.github.com>
2021-05-16 23:48:59 +0530

    Add files via upload

---
+++ screens/InstagramScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+export default class InstagramScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text style={{fontSize:20}}>
+                    INSTAGRAM
+                    </Text>
+                </View>
+        )
+    }
+}
